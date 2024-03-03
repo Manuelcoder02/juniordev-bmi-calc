d031b68f3dfa7ab948333e1ebbd46a6476934942
Sotunde Emmanuel <sotundeemmanuel@gmail.com>
2024-03-03 02:57:54 +0100

    updated work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,16 @@
 // btn - for buttons
 // input - for inputs
 
-const btnMetric = document.querySelector(".btn-metric");
-const btnImperial = document.querySelector(".btn-imperial");
+const btnMetric = document.querySelector(".box-metric");
+const btnImperial = document.querySelector(".box-imperial");
+
+const formMetric = document.querySelector(".form-metric");
+const formImperial = document.querySelector(".form-imperial");
 
 const inputHeightMetric = document.querySelector(".height-metric");
 const inputWeightMetric = document.querySelector(".weight-metric");
 
 const labelBMIScore = document.querySelector(".bmi-score");
+
+// console.log(btnMetric);
+// console.log(btnImperial);
