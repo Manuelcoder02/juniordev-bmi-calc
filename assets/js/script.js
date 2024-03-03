f52744fe41a93ef077c1e753de89b7c10fdbed2b
Sotunde Emmanuel <sotundeemmanuel@gmail.com>
2024-03-03 02:36:04 +0100

    updated work

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+//label - for texts that are going to show
+// btn - for buttons
+// input - for inputs
+
+const btnMetric = document.querySelector(".btn-metric");
+const btnImperial = document.querySelector(".btn-imperial");
